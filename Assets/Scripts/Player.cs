0164e141aaf52adb93ce69f2716913e79b7db7d2
James Spence <Jamesaspence20@gmail.com>
2017-03-25 23:44:08 -0500

    Player script with initial raycaster

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : Agent
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		RaycastHit hit;
+		Ray bullet = new Ray (transform.position, Vector3.up);
+		if (Physics.Raycast (bullet, out hit, 11f)) {
+			GameObject gameObject = hit.collider.gameObject;
+			Debug.Log (gameObject);
+		} else {
+			Debug.Log ("No dice.");
+		}
+		Debug.Log ("Started!");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
+
