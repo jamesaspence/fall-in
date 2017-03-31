0563bf16c97fd23d465d8977fd6f43ede40ff7fd
James Spence <Jamesaspence20@gmail.com>
2017-03-30 23:02:50 -0500

    added unadded, missing files to fix build

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private int targetX = -15;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (TargetNeedsInversion()) {
+	        targetX = -(targetX);
+	    }
+
+	    Vector3 direction;
+	    if (targetX > 0) {
+	        direction = Vector3.right;
+	    } else {
+	        direction = Vector3.left;
+	    }
+        
+        transform.Translate(direction * (4f * Time.deltaTime));
+	}
+
+    private bool TargetNeedsInversion()
+    {
+        Vector3 position = transform.position;
+        if (targetX > 0) {
+            return position.x >= targetX;
+        } else {
+            return position.x <= targetX;
+        }
+    }
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+        //targetPosition.x = transform.position.z;
+        //targetPosition.y = transform.position.y;
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 targetPosition = target.transform.position;
+        transform.LookAt(targetPosition, transform.up);
+    }
+}
